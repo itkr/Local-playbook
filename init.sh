e013cd2a3a734a6551fc4e1e10813086d65e8299
itkr <itkrst@gmail.com>
2016-10-21 12:58:39 +0900

    google cloud sdkの追加とメモの追加

--- init.sh
+++ init.sh
@@ -4,3 +4,10 @@ xcodebuild -license
 xcode-select --install
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew install ansible
+brew install git
+mkdir -p ~/Project
+git clone https://github.com/itkr/Local-playbook.git ~/Project/Local-playbook
+
+# -- Playbook実行後 --
+# iterm2の設定インポート
+# gcloud init
