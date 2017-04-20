76c41e64c7ae50699a43127987bbc5805838ff32
itkr <itkrst@gmail.com>
2017-04-20 17:31:16 +0900

    Update sh

--- init.sh
+++ init.sh
@@ -1,10 +1,18 @@
 # 未検証
 
+# Install Xcode
+# pass
+
+# Activate Xcode
 xcodebuild -license
 xcode-select --install
+
+# Install homebrew
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew install ansible
 brew install git
+
+# Download playbook
 mkdir -p ~/Project
 git clone https://github.com/itkr/Local-playbook.git ~/Project/Local-playbook
 
