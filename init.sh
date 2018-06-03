858a86f6ed1250b30c1afd3232ef36ab4e4b278b
itkr <1531860+itkr@users.noreply.github.com>
2018-06-03 21:25:39 +0900

    Update tasks

--- init.sh
+++ init.sh
@@ -1,8 +1,5 @@
 # 未検証
 
-# Install Xcode
-# pass
-
 # Activate Xcode
 sudo xcodebuild -license
 xcode-select --install
@@ -10,12 +7,7 @@ xcode-select --install
 # Install homebrew
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew install ansible
-brew install git
 
 # Download playbook
-mkdir -p ~/Project
-git clone https://github.com/itkr/Local-playbook.git ~/Project/Local-playbook
-
-# -- Playbook実行後 --
-# iterm2の設定インポート
-# gcloud init
+#mkdir -p ~/.ghq
+#git clone https://github.com/itkr/Local-playbook.git ~/.ghq/github.com/itkr/Local-playbook
